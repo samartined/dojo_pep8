bdc6dd4c6ba2737d880eee1b895a54492e2599cd
Edgar <espinosamartinedgar@gmail.com>
2024-01-29 16:43:40 +0100

    Se arregla totalmente la lógica y se adaptan a la vez las variables y nomenclaturas al estandar.

--- codebreaker.py
+++ codebreaker.py
@@ -1,36 +1,38 @@
 import random
 
+true_number = "".join(random.sample("0123456789", 4))
+
 
 class Codebreaker:
-    trueNumber = random.randint(1000, 9999)
-
-    def adivinar(self, numero=None):
-        if self.trueNumber == "":
+    def adivinar(self, numero=true_number):
+        if true_number == "":
             return "Number is not defined"
 
-        if numero is None or len(numero) != 4 or "e" not in list(numero):
+        if numero is None or len(numero) != 4:
             return "error"
 
-        if numero == self.trueNumber:
+        if numero == true_number:
             return True
 
-        resultadoX = ""
+        resultado_x = ""
         resultado_ = ""
-        arrayNumber = []
+        array_number = [False] * 10
 
-        for x in len(numero):
-            if arrayNumber[numero[x]] == True:
-                return "error"
-
-            arrayNumber[numero[x]] = True
+        for index, x in enumerate(true_number):
+            array_number[int(x)] = True
 
         numero = list(numero)
+        true_number_list = list(true_number)
 
-        for index, x in numero:
-            if self.trueNumber[index] == numero[index]:
-                resultadoX += "X"
+        for index, x in enumerate(numero):
+            if x in true_number_list:
+                if true_number_list[index] == x:
+                    resultado_x += "X"
+                    true_number_list[index] = None
+                else:
+                    resultado_ += "_"
+                    true_number_list[
+                        true_number_list.index(x)
+                    ] = None  # Para evitar que se repita el mismo numero
 
-            elif x in self.trueNumber:
-                resultado_ = "_"
-
-        return resultadoX + resultado_
+        return resultado_x + resultado_
--- main.py
+++ main.py
@@ -5,14 +5,25 @@ codebreaker = Codebreaker()
 
 intento = 0
 
-print('Jugar Codebreaker!')
+print(
+    "Jugar Codebreaker!\nTienes 10 intentos para adivinar el numero de 4 dígitos, "
+    "cada X significa que tienes un numero correcto en la posición correcta y cada _ "
+    "significa que tienes un numero correcto en la posición incorrecta. Suerte!"
+)
 
 while intento != intentos_totales:
-   number = input('Numero:');
-   resolve = codebreaker.adivinar(number)
-   print(resolve)
-   if resolve == True:
-      print('You win!!')
-      break
+    number = input("Numero:")
+    resolve = codebreaker.adivinar(number)
+    print(resolve)
+    if resolve == "XXXX":
+        print("You win!!")
+        break
+    if intento == intentos_totales - 1:
+        print("You lose!!")
+        break
+    if resolve == "error":
+        print("El numero debe tener 4 dígitos")
+    if resolve == "Number is not defined":
+        print("El numero no esta definido")
 
-
+    intento += 1
